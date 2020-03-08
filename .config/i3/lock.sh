3d9d044bf52f38fbd0248bdabb290e6061552d96
Felix Strobel <mail@felixstrobel.de>
2020-03-08 17:51:45 +0100

    [i3] remove unnecessary files

--- .config/i3/lock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-i3lock &
-
-# Turn the screen off after a delay.
-sleep 2
-xset dpms force suspend
--- .config/i3/start-xss.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-xset s 300
-# let xss-lock listen to 'loginctl lock-session'
-xss-lock --transfer-sleep-lock -- /home/worldpotato/repos/dotfiles/i3/lock.sh --no-fork &
