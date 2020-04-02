981737c690764874fd2b872c6e775339caa40a41
Felix Strobel <mail@felixstrobel.de>
2020-04-02 22:13:39 +0200

    [bin] hide the bin folder as .bin

---
+++ .bin/system/autostart.sh
@@ -0,0 +1,17 @@
+#feh -F $HOME/.bin/system/background/desktop_background.png &
+#/usr/bin/picom -CGb & # picom with disabled shadows &
+# bash script to activate slock after some amount of time
+$HOME/.config/polybar/launch.sh & 2&> /dev/null
+$HOME/.bin/system/background/setbg & 2&> /dev/null # set desktop background &
+/usr/bin/nm-applet & 2&> /dev/null
+/usr/bin/dunst &
+
+$HOME/.bin/system/xsidle.sh slock &
+
+numlockx on
+
+# lock the screen after 5 min
+xset s 300
+sleep 2
+/usr/bin/thunderbird & 2&> /dev/null
+/usr/bin/firefox & 2&> /dev/null
