ab3ccd31043239519ffb199a223edc40681cdb36
Felix Strobel <mail@felixstrobel.de>
2020-09-05 20:59:57 +0200

    [xinit] move slock start to autostart script

--- .bin/system/autostart.sh
+++ .bin/system/autostart.sh
@@ -6,12 +6,12 @@ $HOME/.bin/system/setbg & 2&> /dev/null # set desktop background &
 /usr/bin/nm-applet & 2&> /dev/null
 /usr/bin/dunst &
 
-$HOME/.bin/system/xsidle.sh slock &
-
 numlockx on
 
 # lock the screen after 5 min
+$HOME/.bin/system/xsidle.sh slock &
+
 xset s 300
 sleep 2
 /usr/bin/thunderbird & 2&> /dev/null
-/usr/bin/firefox & 2&> /dev/null
+/usr/bin/qutebrowser & 2&> /dev/null
