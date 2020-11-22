8df5181adc660fe69c473e24ed86efc69563d138
Felix Strobel <mail@felixstrobel.de>
2020-11-22 10:46:48 +0100

    [i3] switch to alacritty

--- .config/i3/create-workspace.sh
+++ .config/i3/create-workspace.sh
@@ -2,8 +2,8 @@
 
 i3-msg "workspace ; append_layout ~/.config/i3/workspace-clean.json"
 
-st -c first &
-st -c gotop -e /usr/bin/gotop &
-st -c clock -e /usr/bin/tty-clock -sbcDC2 -f %d.%m.%Y &
+alacritty &
+alacritty -e /usr/bin/gotop &
+alacritty -e /usr/bin/tty-clock -sbcDC2 -f %d.%m.%Y &
 
 i3-msg restart
