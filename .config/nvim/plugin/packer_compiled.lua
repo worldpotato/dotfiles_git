860ff6134a5d7c644bc82d8daf63d5f517877604
Felix Strobel <mail@felixstrobel.de>
2021-12-12 11:40:50 +0100

    add packer compiled

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -57,7 +57,7 @@ end
 time([[Luarocks path setup]], false)
 time([[try_loadstring definition]], true)
 local function try_loadstring(s, component, name)
-  local success, result = pcall(loadstring(s))
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
   if not success then
     vim.schedule(function()
       vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
@@ -69,106 +69,200 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  LuaSnip = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
+  chadtree = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/chadtree",
+    url = "https://github.com/ms-jpq/chadtree"
+  },
   cmake4vim = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/cmake4vim"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/cmake4vim",
+    url = "https://github.com/ilyachur/cmake4vim"
+  },
+  ["coq.artifacts"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/coq.artifacts",
+    url = "https://github.com/ms-jpq/coq.artifacts"
+  },
+  ["coq.thirdparty"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/coq.thirdparty",
+    url = "https://github.com/ms-jpq/coq.thirdparty"
+  },
+  coq_nvim = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/coq_nvim",
+    url = "https://github.com/ms-jpq/coq_nvim"
   },
   ["formatter.nvim"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/formatter.nvim"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/formatter.nvim",
+    url = "https://github.com/mhartington/formatter.nvim"
   },
   ["fzf-lua"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/fzf-lua"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/fzf-lua",
+    url = "https://github.com/ibhagwan/fzf-lua"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/gitsigns.nvim"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["i3-vim-nav"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/i3-vim-nav"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/i3-vim-nav",
+    url = "https://github.com/termhn/i3-vim-nav"
   },
   ["lightline-buffer"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/lightline-buffer"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/lightline-buffer",
+    url = "https://github.com/taohexxx/lightline-buffer"
   },
   ["lightline.vim"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/lightline.vim"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/lightline.vim",
+    url = "https://github.com/itchyny/lightline.vim"
   },
   ["nvim-comment"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-comment"
-  },
-  ["nvim-compe"] = {
-    loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-compe"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-comment",
+    url = "https://github.com/terrortylor/nvim-comment"
   },
   ["nvim-dap"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-dap"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-dap",
+    url = "https://github.com/mfussenegger/nvim-dap"
+  },
+  ["nvim-dap-python"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-dap-python",
+    url = "https://github.com/mfussenegger/nvim-dap-python"
   },
   ["nvim-dap-ui"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-dap-ui"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-dap-ui",
+    url = "https://github.com/rcarriga/nvim-dap-ui"
+  },
+  ["nvim-dap-virtual-text"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-dap-virtual-text",
+    url = "https://github.com/theHamsta/nvim-dap-virtual-text"
   },
   ["nvim-fzf"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-fzf"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-fzf",
+    url = "https://github.com/vijaymarupudi/nvim-fzf"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    url = "https://github.com/neovim/nvim-lspconfig"
+  },
+  ["nvim-terminal"] = {
+    config = { "\27LJ\1\2X\0\0\2\0\6\0\n4\0\0\0007\0\1\0)\1\2\0:\1\2\0004\0\3\0%\1\4\0>\0\2\0027\0\5\0>\0\1\1G\0\1\0\nsetup\18nvim-terminal\frequire\vhidden\6o\bvim\0" },
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-terminal",
+    url = "https://github.com/s1n7ax/nvim-terminal"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-treesitter-refactor"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-treesitter-refactor"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-treesitter-refactor",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-refactor"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/opt/packer.nvim"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/opt/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
   },
   playground = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/playground"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/playground",
+    url = "https://github.com/nvim-treesitter/playground"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["telescope.nvim"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tmux.nvim"] = {
+    config = { "\27LJ\1\2¶\1\0\0\3\0\n\0\r4\0\0\0%\1\1\0>\0\2\0027\0\2\0003\1\4\0003\2\3\0:\2\5\0013\2\6\0:\2\a\0013\2\b\0:\2\t\1>\0\2\1G\0\1\0\vresize\1\0\1\31enable_default_keybindings\2\15navigation\1\0\1\31enable_default_keybindings\2\14copy_sync\1\0\0\1\0\1\venable\2\nsetup\ttmux\frequire\0" },
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/tmux.nvim"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/tmux.nvim",
+    url = "https://github.com/aserowy/tmux.nvim"
+  },
+  ["vim-bazel"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-bazel",
+    url = "https://github.com/bazelbuild/vim-bazel"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-fugitive"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-maktaba"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-maktaba"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-maktaba",
+    url = "https://github.com/google/vim-maktaba"
+  },
+  ["vim-test"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-test",
+    url = "https://github.com/vim-test/vim-test"
   },
   ["vim-tmux-navigator"] = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-tmux-navigator"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-tmux-navigator",
+    url = "https://github.com/christoomey/vim-tmux-navigator"
+  },
+  ["vim-ultest"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-ultest",
+    url = "https://github.com/rcarriga/vim-ultest"
+  },
+  ["vim-wordmotion"] = {
+    loaded = true,
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vim-wordmotion",
+    url = "https://github.com/chaoren/vim-wordmotion"
   },
   vimtex = {
     loaded = true,
-    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vimtex"
+    path = "/home/worldpotato/.local/share/nvim/site/pack/packer/start/vimtex",
+    url = "https://github.com/lervag/vimtex"
   }
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: tmux.nvim
+time([[Config for tmux.nvim]], true)
+try_loadstring("\27LJ\1\2¶\1\0\0\3\0\n\0\r4\0\0\0%\1\1\0>\0\2\0027\0\2\0003\1\4\0003\2\3\0:\2\5\0013\2\6\0:\2\a\0013\2\b\0:\2\t\1>\0\2\1G\0\1\0\vresize\1\0\1\31enable_default_keybindings\2\15navigation\1\0\1\31enable_default_keybindings\2\14copy_sync\1\0\0\1\0\1\venable\2\nsetup\ttmux\frequire\0", "config", "tmux.nvim")
+time([[Config for tmux.nvim]], false)
+-- Config for: nvim-terminal
+time([[Config for nvim-terminal]], true)
+try_loadstring("\27LJ\1\2X\0\0\2\0\6\0\n4\0\0\0007\0\1\0)\1\2\0:\1\2\0004\0\3\0%\1\4\0>\0\2\0027\0\5\0>\0\1\1G\0\1\0\nsetup\18nvim-terminal\frequire\vhidden\6o\bvim\0", "config", "nvim-terminal")
+time([[Config for nvim-terminal]], false)
 if should_profile then save_profiles() end
 
 end)
